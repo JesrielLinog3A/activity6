fb18a9ece43d8cf7bc59d0f95e178cfe371c2452
JesrielLinog3A <143070165+JesrielLinog3A@users.noreply.github.com>
2023-11-22 18:34:09 +0800

    Add files via upload

---
+++ src/Users.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export const Users = [
+
+    {
+        id: 1,
+        name:{
+            firstname: "Ryan",
+            lastname: "Pajardo",
+        },
+        course:"BSCS",
+        yearsection: "2A",
+        username: "Pajardo",
+        password: "ryan123",
+        usertype: "Student",
+    },
+    {
+        id: 2,
+        name:{
+            firstname: "Arnel",
+            lastname: "Pjota",
+        },
+        course:"BSIT",
+        yearsection: "3B",
+        username: "Arnel06", 
+        password: "nel143",
+        usertype: "Student",
+    },
+    {
+        id: 3,
+        name:{
+            firstname: "Rhea",
+            lastname: "Abne",
+        },
+        course:"BSCRIM",
+        yearsection: "3A",
+        username: "rhea02",
+        password: "yang45",
+        usertype: "Student",
+    },
+    {
+        id: 4,
+        name:{
+            firstname: "Gabrene",
+            lastname: "Diaz",
+        },
+        course:"",
+        yearsection: "",
+        username: "diaz91",
+        password: "12345",
+        usertype: "Teacher",
+    },
+    {
+        id: 5,
+        name:{
+            firstname: "Jerry",
+            lastname: "Ale",
+        },
+        course:"",
+        yearsection: "",
+        username: "jery",
+        password: "ale123",
+        usertype: "Teacher",
+    },
+    
+];
